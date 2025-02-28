5d8576cd3675b766322d234f9ade43cab69dc468
LeoDreamer <2200010825@stu.pku.edu.cn>
2025-02-28 16:07:56 +0800

    rebuilding site 2025年 02月 28日 星期五 16:07:56 CST

---
+++ post/rewrite.py
@@ -0,0 +1,20 @@
+import sys
+
+filepath = sys.argv[1]
+with open(filepath, 'r') as f:
+    content = f.read()
+    
+content = content.replace(
+    r"\thm",
+    """<div class="block">\n\n<p class="block-title thm">定理</p>"""
+).replace(
+    r"\def",
+    """<div class="block">\n\n<p class="block-title def">定义</p>"""
+).replace(
+    r"\end", "</div>"
+).replace(
+    r"\R", r"\mathbb{R}"
+)
+
+with open(filepath, 'w') as f:
+    f.write(content)
